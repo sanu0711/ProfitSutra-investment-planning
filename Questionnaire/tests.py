5ea0289990cd38b884bae3982bbf7bd2bdd4b7f9
ABHISHEK YADAV <sanuabhi.0711@gmail.com>
2025-01-04 14:06:29 +0530

    testing bot

--- Questionnaire/tests.py
+++ Questionnaire/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+# def gen_ai_response(question):
+#     prompt =f""" 
+#     This is question asked by user:{question}./n
+#     Provide detailed information about use question, if it is related to stock including its current price, historical performance (1 month, 6 months, 1 year), market capitalization, P/E ratio, dividend yield, key financials, recent news, and analyst recommendations. Highlight any significant trends or news that might impact its future performance
+#     return the response in markdown format
+#     """
+#     genai.configure(api_key=os.getenv("GENAI_API_KEY"))
+#     model = genai.GenerativeModel("gemini-1.5-flash")
+#     response = model.generate_content(question)
+#     # markdown2.markdown(response.text)
+#     return response.text
--- Questionnaire/views.py
+++ Questionnaire/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from .chatbot import chat_to_query,response_q
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def chatbot_view(request):
+    # response = chat_to_query('top 10 stocks having highest marketcap')
+    # response= response_q()
+    # print(response)
+    if request.method == 'POST':
+        question = request.POST.get('question')
+        if question:
+            response = response_q(question)
+            return render(request, 'chatbot.html', {
+                'response': response,
+                'qus': question})
+        else:
+            messages.error(request, "Please enter a question to get response")
+            return render(request, 'chatbot.html')    
     return render(request, 'chatbot.html')
+  
 
 def questionnaire_view(request):
     if request.method == 'POST':
