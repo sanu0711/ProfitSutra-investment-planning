89fdcd047dfa4e5eeff8da1254c8b11fb6ad12be
ABHISHEK YADAV <sanuabhi.0711@gmail.com>
2025-01-15 15:25:42 +0530

    recommendation ticker with backend

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render,redirect
 from .models import Portfolio, UserStock
 from django.contrib.auth.decorators import login_required
 from dashboard.models import StockData
+import requests
+import json
 # Create your views here.
+
+
+def recom_helper():
+    data = []
+    tickers = ['20MICRONS','360ONE','3MINDIA','INFOLLION','TDPOWERSYS']
+    for ticker in tickers:
+        url = f"http://20.197.4.173/stock_details/{ticker}"
+        response = requests.get(url)
+        data.append(json.loads(response.json()))
+       
+    return data
 
 @login_required(login_url='sign_in') 
 def portfolio_view(request):
@@ -12,10 +25,12 @@ def portfolio_view(request):
         portfolio.save()
     stock_names_option = StockData.objects.values_list('stock_name', flat=True)
     user_stocks = portfolio.stocks.all()
+    recom = recom_helper()
     return render(request, 'portfolio.html', {
         'portfolio': portfolio,
         'stock_names_option': stock_names_option,
         'user_stocks': user_stocks,
+        'recom': recom,
         })
 
 @login_required(login_url='sign_in') 
